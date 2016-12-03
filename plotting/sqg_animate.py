b8ccaa5c56c0dc43c2611ca2b4d59c5ad54b9a05
Jeff Whitaker <jswhit@fastmail.fm>
2016-12-03 14:47:04 -0700

    new animation script

---
+++ plotting/sqg_animate.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from netCDF4 import Dataset
+
+filename = '../data/sqg_N512.nc'
+
+nc = Dataset(filename)
+pv_var = nc['pv']
+t_var = nc['t']
+nsteps = len(t_var)
+scalefact = nc.f*nc.theta0/nc.g
+
+fig = plt.figure(figsize=(8,8))
+fig.subplots_adjust(left=0, bottom=0.0, top=1., right=1.)
+vmin = -30; vmax = 30; nout = 1; levplot = 1
+
+def initfig():
+    global im
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    pv = scalefact*pv_var[0,levplot,...]
+    im = ax.imshow(pv,interpolation='nearest',origin='lower',vmin=vmin,vmax=vmax)
+    return im,
+
+def updatefig(*args):
+    global nout
+    t = t_var[nout]
+    pv = scalefact*pv_var[nout]
+    hr = t/3600.
+    print hr,pv.min(),pv.max()
+    im.set_data(pv[levplot])
+    nout = nout + 1
+    return im,
+
+# interval=0 means draw as fast as possible
+ani = animation.FuncAnimation(fig, updatefig, frames=nsteps, repeat=False,\
+      init_func=initfig,interval=0,blit=True)
+plt.show()
