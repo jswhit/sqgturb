c0c6aee046f3b8ebc24d013262b351f140f0e149
Jeff Whitaker <jswhit@fastmail.fm>
2016-12-07 11:58:58 -0700

    add enkf_utils to main package

--- sqgturb/__init__.py
+++ sqgturb/__init__.py
@@ -17,5 +17,6 @@ time stepping (dealiasing with 2/3 rule, hyperdiffusion treated implicitly).
 Jeff Whitaker December, 2016 <jeffrey.s.whitaker@noaa.gov>
 """
 from sqg import SQG,  rfft2, irfft2
+import enkf_utils
 
-__all__ = ['SQG','rfft2','irfft2']
+__all__ = ['SQG','rfft2','irfft2','enkf_utils']
