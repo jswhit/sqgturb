69257ae100a5a28df8872411ef5b99ede3950eae
Jeff Whitaker <jswhit@fastmail.fm>
2016-12-07 10:10:03 -0700

    start reorg

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+short_desc = "A program for simulating surface quasi-geostropic turbulence"
+setup(
+  name = 'sqgturb',
+  version = '0.1',
+  description = short_desc,
+  author = 'Jeff Whitaker',
+  author_email = 'jeffrey dot s dot whitaker at noaa dot gov',
+  url = 'https://github.com/jswhit/sqgturb',
+  packages = ['sqgturb'],
+  requires = ['numpy','pyfftw']
+)
+
+
+
+
+
+
+
+
---
+++ sqgturb/__init__.py
@@ -0,0 +1,21 @@
+"""
+constant PV f-plane QG turbulence (a.k.a surface QG turbulence).
+Doubly periodic geometry with sin(2*pi/L) jet basic state.
+References:
+http://journals.ametsoc.org/doi/pdf/10.1175/2008JAS2921.1 (section 3)
+http://journals.ametsoc.org/doi/pdf/10.1175/1520-0469%281978%29035%3C0774%3AUPVFPI%3E2.0.CO%3B2
+
+includes Ekman damping, linear thermal relaxation back
+to equilibrium jet, and hyperdiffusion.
+
+pv has units of meters per second.
+scale by f*theta0/g to convert to temperature.
+
+FFT spectral collocation method with 4th order Runge Kutta
+time stepping (dealiasing with 2/3 rule, hyperdiffusion treated implicitly).
+
+Jeff Whitaker December, 2016 <jeffrey.s.whitaker@noaa.gov>
+"""
+from sqg import SQG,  rfft2, irfft2
+
+__all__ = ['SQG','rfft2','irfft2']
