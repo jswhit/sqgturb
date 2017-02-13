92bf1015382dafd0256414152b7fb86847ad010c
Jeff Whitaker <jswhit@fastmail.fm>
2017-02-12 19:38:52 -0700

    add comments

--- sqgturb/randompattern.py
+++ sqgturb/randompattern.py
@@ -8,6 +8,15 @@ class RandomPattern:
         define an ensemble of random patterns with specified temporal
         and spatial covariance structure by applying Gaussian blur to
         white noise.
+        spatial_corr_efold:  horizontal efolding scale for 
+        isotropic spatial correlation structure.
+        temporal_corr_efold:  efolding time scale for temporal
+        correlation.
+        L: size of square domain (m)
+        N: number of grid points in each periodic direction
+        dt: time step to evolve paptern.
+        nsamples:  number of ensemble members.
+        stdev:  spatial standard deviation (amplitude).
         """
         self.hcorr = spatial_corr_efold
         self.tcorr = temporal_corr_efold
@@ -33,16 +42,20 @@ class RandomPattern:
 
     def evolve(self):
         """
-        evolve sample one time step
+        evolve random patterns one time step
         """
+        # generate white noise.
         newpattern = self.stdev*np.random.normal(\
                      size=(self.nsamples,self.N,self.N))
+        # apply gaussian filter
         for n in range(self.nsamples):
             newpattern[n] = gaussian_filter(newpattern[n], 
             self.filter_stdev, order=0, output=None,
             mode='wrap', cval=0.0, truncate=4.0)
+        # restore variance removed by gaussian blur.
         newpattern =\
         newpattern*(self.filter_stdev*2.*np.sqrt(np.pi))
+        # blend new pattern with old pattern.
         self.pattern = \
         np.sqrt(1.-self.lag1corr**2)*newpattern + \
         self.lag1corr*self.pattern
@@ -82,7 +95,6 @@ if __name__ == "__main__":
         spatial_cov = spatial_cov + x[rp.N/2,rp.N/2]*x/(ntimes-1)
     plt.figure()
     x = (rp.L/rp.N)*np.arange(rp.N)-rp.L/2
-    print x.shape, spatial_cov.shape
     plt.plot(x,spatial_cov[:,rp.N/2],'r')
     plt.plot(x,np.exp(-(x/rp.hcorr)**2),'k')
     plt.axhline(0); plt.axvline(0)
