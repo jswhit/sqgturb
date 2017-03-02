08f5e65b8dfae69dccaa1a8bf08e229f36ba81d5
jswhit <whitaker.jeffrey@gmail.com>
2017-03-02 22:25:13 +0000

    remove dead code

--- sqgturb/sqg.py
+++ sqgturb/sqg.py
@@ -199,8 +199,7 @@ class SQG:
         # add sub-grid scale stochastic component to advection.
         # (held constant over RK4 time step)
         if self.random_pattern is not None:
-            # generate random streamfunction field
-            # for this RK4 substep
+            # generate random streamfunction field,
             # compute perturbation u,v for randomized advection.
             if self.rkstep == 0:
                 # assume random winds constant over RK4 step
@@ -209,33 +208,6 @@ class SQG:
                 ke = 0.5*(self.upert**2+self.vpert**2).mean()
                 self.diffcoeff = ke/self.dt
                 self.random_pattern.evolve()
-            #if self.rkstep == 0:
-            #    psispec_pert0 = rfft2(self.random_pattern.pattern)
-            #    self.random_pattern.evolve(dt=0.5*self.dt)
-            #    psispec_pert1 = rfft2(self.random_pattern.pattern)
-            #    self.random_pattern.evolve(dt=0.5*self.dt)
-            #    psispec_pert2 = rfft2(self.random_pattern.pattern)
-            #    # winds at beginning, middle and end of RK4 step
-            #    self.vpert0, self.upert0 = self.xyderiv(psispec_pert0); self.upert0 = -self.upert0
-            #    self.vpert1, self.upert1 = self.xyderiv(psispec_pert1); self.upert1 = -self.upert1
-            #    self.vpert2, self.upert2 = self.xyderiv(psispec_pert2); self.upert2 = -self.upert2
-            #    upert = self.upert0
-            #    vpert = self.vpert0
-            #    # horizontally homogeneous diffusion to balance random advection
-            #    # (since it is homogenous, there is no drift correction to upert,vpert)
-            #    # this ensures variance of tracer not changed by randomized transport
-            #    ke0 = 0.5*(self.upert0**2+self.vpert0**2).mean()
-            #    ke1 = 0.5*(self.upert1**2+self.vpert1**2).mean()
-            #    ke2 = 0.5*(self.upert2**2+self.vpert2**2).mean()
-            #    self.diffcoeff = (1./3.)*(ke0+ke1+ke2)/self.dt
-            #if self.rkstep in [1,2]:
-            #    upert = self.upert1
-            #    vpert = self.vpert1
-            #elif self.rkstep == 3:
-            #    upert = self.upert2
-            #    vpert = self.vpert2
-            #u += upert
-            #v += vpert
             u += self.upert
             v += self.vpert
         advection = u*pvx + v*pvy
