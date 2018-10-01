86992fe374e0f3fe32a2da7ec79aa8c0affbb1f6
Jeff Whitaker <jswhit@fastmail.fm>
2018-10-01 09:50:04 -0600

    add new files

---
+++ run_ensemble.py
@@ -0,0 +1,207 @@
+from netCDF4 import Dataset
+import numpy as np
+import sys, os
+from sqgturb import SQG, rfft2, irfft2
+from scipy.ndimage import gaussian_filter
+
+# get OMP_NUM_THREADS (threads to use) from environment.
+threads = int(os.getenv('OMP_NUM_THREADS','1'))
+
+verbose=False
+if len(sys.argv) < 2:
+    print 'python run_ensemble.py filenamein amp filename_inc'
+    raise SystemExit
+
+filenamein = sys.argv[1] # upscaled truth
+amp = float(sys.argv[2]) # amplitude of additive noise
+#amp = 0.
+filename_inc = sys.argv[3] # analysis increments
+#filename_inc = None
+# smoothing applied to truth and forecasts.
+#stdev = 1.-np.exp(-1)
+stdev = None # no smoothing
+
+diff_efold_ens=86400.  ; dt = 1200 #N64
+#diff_efold_ens=86400./2.; dt = 600  #N128
+#diff_efold_ens=86400./2.; dt = 240  #N256
+
+diff_efold_det = diff_efold_ens
+nanals = 20
+
+nc = Dataset(filenamein)
+scalefact = nc.f*nc.theta0/nc.g
+# initialize qg model instance
+pv = nc['pv'][0]
+norder = 8
+N = pv.shape[-1]
+models = []
+for nanal in range(nanals):
+    models.append( SQG(pv,nsq=nc.nsq,f=nc.f,U=nc.U,H=nc.H,r=nc.r,tdiab=nc.tdiab,dt=dt,
+                   diff_order=norder,diff_efold=diff_efold_ens,
+                   ai_amp=amp,ai_filename=filename_inc,
+                   dealias=bool(nc.dealias),symmetric=bool(nc.symmetric),threads=threads,
+                   precision='single') )
+modeld = SQG(pv,nsq=nc.nsq,f=nc.f,U=nc.U,H=nc.H,r=nc.r,tdiab=nc.tdiab,dt=dt,
+             diff_order=norder,diff_efold=diff_efold_det,
+             dealias=True,symmetric=bool(nc.symmetric),threads=threads,
+             precision='single')
+print '# amp, filename_inc = ',amp,filename_inc
+fcstlenmax = 80
+nfcsts = 200
+fcstlenmax = 24
+nfcsts = 20
+fcstleninterval = 1
+fcstlenspectra = [1,4,8,10,20]
+fcsttimes = fcstlenmax/fcstleninterval
+outputinterval = fcstleninterval*(nc['t'][1]-nc['t'][0])
+print '# output interval = ',outputinterval
+forecast_timesteps = int(outputinterval/models[nanal].dt)
+#modeld.timesteps = int(outputinterval/modeld.dt)
+scalefact = nc.f*nc.theta0/nc.g
+ntimes = len(nc.dimensions['t'])
+
+N = modeld.N
+pverrsq_mean = np.zeros((fcsttimes,2,N,N),np.float)
+pverrsqd_mean = np.zeros((fcsttimes,2,N,N),np.float)
+pvspread_mean = np.zeros((fcsttimes,2,N,N),np.float)
+pvens = np.zeros((nanals,2,N,N),np.float)
+kespec_errmean = np.zeros((fcsttimes,2,N,N/2+1),np.float)
+kespec_sprdmean = np.zeros((fcsttimes,2,N,N/2+1),np.float)
+nskip = 8
+ntimes = nfcsts*nskip+fcstlenmax # for debugging
+print '# ntimes,nfcsts,fcstlenmax,nskip = ',ntimes,nfcsts,fcstlenmax,nskip
+ncount = len(range(0,ntimes-fcstlenmax,nskip))
+print '# ',ncount,'forecasts',fcsttimes,'forecast times',forecast_timesteps,\
+      'time steps for forecast interval'
+print '# amp = %s' % amp
+
+for n in range(0,ntimes-fcstlenmax,nskip):
+    pvspecic = rfft2(nc['pv'][n])
+    for nanal in range(nanals):
+        models[nanal].pvspec = pvspecic
+    modeld.pvspec = pvspecic
+
+    #pvfcstd = irfft2(modeld.pvspec)
+    #pvtruth = nc['pv'][n]
+    #pverrsqd = (scalefact*(pvfcstd - pvtruth))**2
+    #print np.sqrt(pverrsqd.mean())
+    #raise SystemExit
+
+    for nfcst in range(fcsttimes):
+        fcstlen = (nfcst+1)*fcstleninterval
+        for nt in range(forecast_timesteps):
+            for nanal in range(nanals):
+                models[nanal].timestep()
+            modeld.timestep()
+        for nanal in range(nanals):
+            pvens[nanal] = irfft2(models[nanal].pvspec)
+
+        #psispecd = modeld.invert(modeld.pvspec)
+        #psispec_truth = modeld.invert(rfft2(nc['pv'][n+fcstlen]))
+        #psid = irfft2(psispecd); psitruth = irfft2(psispec_truth)
+
+        pvfcstd = irfft2(modeld.pvspec)
+        pvfcstmean = pvens.mean(axis=0)
+        pvtruth = nc['pv'][n+fcstlen]
+
+        # smooth truth and forecasts
+        if stdev is not None:
+           for k in range(2):
+               pvtruth[k] = gaussian_filter(pvtruth[k],stdev,output=None,
+                            order=0,mode='wrap', cval=0.0)
+               pvfcstd[k] = gaussian_filter(pvfcstd[k],stdev,output=None,
+                            order=0,mode='wrap', cval=0.0)
+               for nanal in range(nanals):
+                   pvens[nanal,k,...] = gaussian_filter(pvens[nanal,k,...],stdev,output=None,
+                                        order=0,mode='wrap', cval=0.0)
+           pvfcstmean = pvens.mean(axis=0)
+
+        pverrsq = (scalefact*(pvfcstmean - pvtruth))**2
+        pverrsqd = (scalefact*(pvfcstd - pvtruth))**2
+        pvspread = ((scalefact*(pvens-pvfcstmean))**2).sum(axis=0)/(nanals-1)
+        #pverrsqd = (psid - psitruth)**2
+
+        # vertical mean PV
+        #pverrsq = (scalefact*(pvfcstmean.mean(axis=0) - pvtruth.mean(axis=0)))**2
+        #pverrsqd = (scalefact*(pvfcstd.mean(axis=0) - pvtruth.mean(axis=0)))**2
+        #pvspread = ((scalefact*(pvens.mean(axis=1)-pvfcstmean.mean(axis=0)))**2).sum(axis=0)/(nanals-1)
+
+        if verbose: print n,fcstlen,np.sqrt(pverrsq.mean()),np.sqrt(pverrsqd.mean()),np.sqrt(pvspread.mean())
+        #if nfcst == 8:
+        #   import matplotlib.pyplot as plt
+        #   x = nc['x'][:]; y = nc['y'][:]
+        #   err = scalefact*(pvtruth-pvfcstd)[1]
+        #   errmax = max(np.abs(err.max()), np.abs(err.min()))
+        #   print err.min(), err.max(), errmax
+        #   plt.figure()
+        #   plt.imshow(scalefact*pvtruth[1],cmap=plt.cm.jet,interpolation='nearest',origin='lower',vmin=-25,vmax=25)
+        #   plt.title('truth')
+        #   plt.axis('off')
+        #   plt.figure()
+        #   plt.imshow(scalefact*pvfcstd[1],cmap=plt.cm.jet,interpolation='nearest',origin='lower',vmin=-25,vmax=25)
+        #   plt.title('fcst')
+        #   plt.axis('off')
+        #   plt.figure()
+        #   plt.imshow(err,cmap=plt.cm.bwr,interpolation='nearest',origin='lower',vmin=-errmax,vmax=errmax)
+        #   plt.title('error')
+        #   plt.axis('off')
+        #   plt.show()
+        #   raise SystemExit
+        pvspread_mean[nfcst] += pvspread/ncount
+        pverrsq_mean[nfcst] += pverrsq/ncount
+        pverrsqd_mean[nfcst] += pverrsqd/ncount
+
+        #if fcstlen in fcstlenspectra:
+        #    pverrspec = scalefact*rfft2(pvfcstmean - pvtruth)
+        #    psispec = modeld.invert(pverrspec)
+        #    psispec = psispec/(modeld.N*np.sqrt(2.))
+        #    kespec = (modeld.ksqlsq*(psispec*np.conjugate(psispec))).real
+        #    kespec_errmean[nfcst] += kespec/ncount
+
+        #    for nanal in range(nanals):
+        #        pvsprdspec = scalefact*rfft2(pvens[nanal] - pvfcstmean)
+        #        psispec = modeld.invert(pvsprdspec)
+        #        psispec = psispec/(modeld.N*np.sqrt(2.))
+        #        kespec = (modeld.ksqlsq*(psispec*np.conjugate(psispec))).real
+        #        kespec_sprdmean[nfcst] += kespec/(nanals*ncount)
+
+#print 'fcstlen = ',fcstlen, 'mean error =',np.sqrt(pverrsq_mean.mean()),np.sqrt(pverrsqd_mean.mean()),np.sqrt(pvspread_mean.mean())
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+for nfcst in range(fcsttimes):
+    fcstlen = (nfcst+1)*fcstleninterval
+    print fcstlen,np.sqrt(pverrsq_mean[nfcst].mean()),np.sqrt(pverrsqd_mean[nfcst].mean()),np.sqrt(pvspread_mean[nfcst].mean())
+    #if fcstlen in fcstlenspectra:
+    #    #vmin = 0; vmax = 4
+    #    #im = plt.imshow(np.sqrt(pvspread_mean[1]),cmap=plt.cm.hot_r,interpolation='nearest',origin='lower',vmin=vmin,vmax=vmax)
+    #    #plt.title('mean spread')
+    #    #plt.figure()
+    #    #im = plt.imshow(np.sqrt(pverrsq_mean[1]),cmap=plt.cm.hot_r,interpolation='nearest',origin='lower',vmin=vmin,vmax=vmax)
+    #    #plt.title('mean error')
+
+    #    k = np.abs((N*np.fft.fftfreq(N))[0:(N/2)+1])
+    #    l = N*np.fft.fftfreq(N)
+    #    k,l = np.meshgrid(k,l)
+    #    ktot = np.sqrt(k**2+l**2)
+    #    ktotmax = N/2+1
+    #    kespec_err = np.zeros(ktotmax,np.float)
+    #    kespec_sprd = np.zeros(ktotmax,np.float)
+    #    for i in range(kespec_errmean[nfcst].shape[2]):
+    #        for j in range(kespec_errmean[nfcst].shape[1]):
+    #            totwavenum = ktot[j,i]
+    #            if int(totwavenum) < ktotmax:
+    #                kespec_err[int(totwavenum)] = kespec_err[int(totwavenum)] +\
+    #                kespec_errmean[nfcst,:,j,i].mean(axis=0)
+    #                kespec_sprd[int(totwavenum)] = kespec_sprd[int(totwavenum)] +\
+    #                kespec_sprdmean[nfcst,:,j,i].mean(axis=0)
+    #    plt.figure()
+    #    wavenums = np.arange(ktotmax,dtype=np.float)
+    #    wavenums[0] = 1.
+    #    idealke = 2.*kespec_err[1]*wavenums**(-5./3,)
+    #    plt.loglog(wavenums,kespec_err,color='k')
+    #    plt.loglog(wavenums,kespec_sprd,color='b')
+    #    #plt.loglog(wavenums,idealke,color='r')
+    #    plt.title('error (black) and spread (blue) spectra for hr %s' %\
+    #            int(3*fcstlen),fontsize=12)
+    #    plt.savefig('%sherr_spectrum_amp%s.png' % (3*fcstlen,amp))
---
+++ upscale.py
@@ -0,0 +1,108 @@
+from netCDF4 import Dataset
+import numpy as np
+import sys
+from sqg import rfft2, irfft2
+from scipy import ndimage
+
+def block_mean(ar, fact):
+    # downsample 2d array by averaging fact x fact blocks
+    # requires scipy.ndimage
+    #assert isinstance(fact, int), type(fact)
+    sx, sy = ar.shape
+    X, Y = np.ogrid[0:sx, 0:sy]
+    regions = sy/fact * (X/fact) + Y/fact
+    res = ndimage.mean(ar, labels=regions,
+                       index=np.arange(regions.max() + 1))
+    res.shape = (sx/fact, sy/fact)
+    return res
+
+def spectrunc(specarr,N):
+    fact = float(N)/float(specarr.shape[1])
+    specarr_trunc = np.zeros((2, N, N/2+1), specarr.dtype)
+    specarr_trunc[:,0:N/2,0:N/2] = fact**2*specarr[:,0:N/2,0:N/2]
+    specarr_trunc[:,-N/2:,0:N/2] = fact**2*specarr[:,-N/2:,0:N/2]
+    return specarr_trunc
+
+# spectrally truncate or block average data in filenamein, write to filenameout on Nout x Nout
+# grid.
+filenamein = sys.argv[1]
+filenameout = sys.argv[2]
+Nout = int(sys.argv[3])
+blockmean = bool(int(sys.argv[4]))
+print('Nout, blockmean = ',Nout,blockmean)
+
+ncin = Dataset(filenamein)
+nc = Dataset(filenameout, mode='w', format='NETCDF4_CLASSIC')
+nc.r = ncin.r
+nc.f = ncin.f
+nc.U = ncin.U
+nc.L = ncin.L
+nc.H = ncin.H
+nc.g = ncin.g; nc.theta0 = ncin.theta0
+nc.nsq = ncin.nsq
+nc.tdiab = ncin.tdiab
+nc.dt = ncin.dt
+nc.diff_efold = ncin.diff_efold
+nc.diff_order = ncin.diff_order
+nc.symmetric = ncin.symmetric
+nc.dealias = ncin.dealias
+x = nc.createDimension('x',Nout)
+y = nc.createDimension('y',Nout)
+z = nc.createDimension('z',2)
+t = nc.createDimension('t',None)
+pvvar =\
+nc.createVariable('pv',np.float32,('t','z','y','x'),zlib=True)
+pvvar.units = 'K'
+# pv scaled by g/(f*theta0) so du/dz = d(pv)/dy
+xvar = nc.createVariable('x',np.float32,('x',))
+xvar.units = 'meters'
+yvar = nc.createVariable('y',np.float32,('y',))
+yvar.units = 'meters'
+zvar = nc.createVariable('z',np.float32,('z',))
+zvar.units = 'meters'
+tvar = nc.createVariable('t',np.float32,('t',))
+tvar.units = 'seconds'
+xvar[:] = np.arange(0,ncin.L,ncin.L/Nout)
+yvar[:] = np.arange(0,ncin.L,ncin.L/Nout)
+zvar[0] = 0; zvar[1] = ncin.H
+N = ncin['pv'].shape[-1]
+print N,Nout
+nskip = N/Nout
+print('nskip = ',nskip)
+ntimes = len(ncin.dimensions['t'])
+#ntimes = 10
+for n in range(ntimes):
+    tvar[n] = ncin['t'][n]
+    pvin = ncin['pv'][n]
+    # downsample by averaging nskip x nskip blocks of pixels
+    if blockmean:
+       pvout = np.empty((2,Nout,Nout), dtype=pvin.dtype)
+       for k in range(2):
+           pvout[k,:,:] = block_mean(pvin[k],nskip)
+    # spectrally truncate.
+    else:
+       pvout = irfft2(spectrunc(rfft2(pvin),Nout))
+    pvvar[n] = pvout
+    print n,tvar[n]/86400.,pvin.shape,pvout.shape,pvin.min(),pvin.max(),pvout.min(),pvout.max()
+nc.close()
+scalefact = ncin.f*ncin.theta0/ncin.g
+ncin.close()
+
+# make plot
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(16,8))
+vmin = -25; vmax= 25
+ax = fig.add_subplot(1,2,1)
+ax.axis('off')
+im = plt.imshow(scalefact*pvin[1],cmap=plt.cm.jet,interpolation='nearest',origin='lower',vmin=vmin,vmax=vmax)
+plt.title('%s x %s solution' % (N,N) ,fontsize=18,fontweight='bold')
+ax = fig.add_subplot(1,2,2)
+ax.axis('off')
+im = plt.imshow(scalefact*pvout[1],cmap=plt.cm.jet,interpolation='nearest',origin='lower',vmin=vmin,vmax=vmax)
+if blockmean:
+   plt.title('upscaled to %s x %s (block mean)' % (Nout,Nout),fontsize=18,fontweight='bold')
+else:
+   plt.title('upscaled to %s x %s (spectrally truncated)' % (Nout, Nout),fontsize=18,fontweight='bold')
+plt.tight_layout()
+plt.savefig('upscale.png')
+plt.show()
