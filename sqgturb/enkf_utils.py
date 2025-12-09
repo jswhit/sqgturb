66b36c1ce42fe7dcc34289b50dbb9a6708bcb88d
jswhit2 <jwhitaker@Jeffreys-MacBook-Air.local>
2025-12-09 13:09:36 -0700

    add lgetkf with cross-validation

--- sqgturb/enkf_utils.py
+++ sqgturb/enkf_utils.py
@@ -248,3 +248,81 @@ def lgetkf_update(hxens, hxens2, obs, oberrs, covlocal):
             enspert_wts[:,:,k,n] = wts_ensperts
 
     return ensmean_wts,enspert_wts
+
+def lgetkf(xens, hxens, obs, oberrs, covlocal):
+
+    """returns ensemble updated by LGETKF with cross-validation"""
+
+    # TODO: implement modulated ensemble
+
+    hxmean = hxens.mean(axis=0)
+    hxprime = hxens - hxmean
+    nanals = hxens.shape[0]
+    ndim = covlocal.shape[-1]
+    xmean = xens.mean(axis=0)
+    xprime = xens - xmean
+    xprime_b = xprime.copy()
+
+    def calcwts_mean(nens, hx, Rinv, ominusf):
+        normfact = np.array(np.sqrt(nens-1),dtype=np.float32)
+        # gain-form etkf solution
+        # HZ^T = hxens * R**-1/2
+        # compute eigenvectors/eigenvalues of A = HZ^T HZ (C=left SV)
+        # (in Bishop paper HZ is nobs, nanals, here is it nanals, nobs)
+        # normalize so dot product is covariance
+        YbsqrtRinv = hx*np.sqrt(Rinv)/normfact
+        YbRinv = hx*Rinv/normfact
+        a = np.dot(YbsqrtRinv,YbsqrtRinv.T)
+        evals, evecs, info = lapack.dsyevd(a)
+        evals = evals.clip(min=np.finfo(evals.dtype).eps)
+        gamma_inv = 1./evals
+        # gammapI used in calculation of posterior cov in ensemble space
+        gammapI = evals+1.
+        # compute factor to multiply with model space ensemble perturbations
+        # to compute analysis increment (for mean update).
+        # This is the factor C (Gamma + I)**-1 C^T (HZ)^ T R**-1/2 (y - HXmean)
+        # in Bishop paper (eqs 10-12).
+        # pa = C (Gamma + I)**-1 C^T (analysis error cov in ensemble space)
+        pa = np.dot(evecs/gammapI[np.newaxis,:],evecs.T)
+        # wts_ensmean = C (Gamma + I)**-1 C^T (HZ)^ T R**-1/2 (y - HXmean)
+        return np.dot(pa, np.dot(YbRinv,ominusf))/normfact
+
+    def calcwts_perts(hx_orig, hx, Rinv):
+        normfact = np.array(np.sqrt(hx_orig.shape[0]-1),dtype=np.float32)
+        # gain-form etkf solution
+        # HZ^T = hxens * R**-1/2
+        # compute eigenvectors/eigenvalues of A = HZ^T HZ (C=left SV)
+        # (in Bishop paper HZ is nobs, nanals, here is it nanals, nobs)
+        # normalize so dot product is covariance
+        YbsqrtRinv = hx*np.sqrt(Rinv)/normfact
+        YbRinv = hx*Rinv/normfact
+        a = np.dot(YbsqrtRinv,YbsqrtRinv.T)
+        evals, evecs, info = lapack.dsyevd(a)
+        evals = evals.clip(min=np.finfo(evals.dtype).eps)
+        gamma_inv = 1./evals
+        # gammapI used in calculation of posterior cov in ensemble space
+        gammapI = evals+1.
+        # compute factor to multiply with model space ensemble perturbations
+        # to compute analysis increment (for perturbation update), save in single precision.
+        # This is -C [ (I - (Gamma+I)**-1/2)*Gamma**-1 ] C^T (HZ)^T R**-1/2 HXprime
+        # in Bishop paper (eqn 29).
+        pa=np.dot(evecs*(1.-np.sqrt(1./gammapI[np.newaxis,:]))*gamma_inv[np.newaxis,:],evecs.T)
+        # wts_ensperts = -C [ (I - (Gamma+I)**-1/2)*Gamma**-1 ] C^T (HZ)^T R**-1/2 HXprime
+        return -np.dot(pa, np.dot(YbRinv,hx_orig.T)).T/normfact # use orig ens here
+
+    for n in range(ndim):
+        mask = covlocal[:,n] > 1.0e-10
+        Rinv = covlocal[mask, n] / oberrs[mask]
+        ominusf = (obs-hxmean)[mask]
+        wts_ensmean = calcwts_mean(nanals, hxprime[:, mask], Rinv, ominusf)
+        for k in range(2):
+            xmean[k,n] += np.dot(wts_ensmean,xprime_b[:,k,n])
+        # update one member at a time, using cross validation.
+        for nanal_cv in range(nanals):
+            hxprime_cv = np.delete(hxprime,nanal_cv,axis=0); xprime_cv = np.delete(xprime_b,nanal_cv,axis=0)
+            wts_ensperts_cv = calcwts_perts(hxprime[nanal_cv,mask], hxprime_cv[:,mask], Rinv)
+            for k in range(2):
+                xprime[nanal_cv,k,n] += np.dot(wts_ensperts_cv,xprime_cv[:,k,n])
+        xens[:,:,n] = xmean[:,n]+xprime[:,:,n]
+
+    return xens
