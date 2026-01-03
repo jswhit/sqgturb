b2ec8a488efd5f8ff2a483c6941d7a8db0c8e4ec
Jeffrey S Whitaker <jwhitake@hercules-login-4.hpc.msstate.edu>
2026-01-03 14:25:52 -0600

    update

--- sqg_run.py
+++ sqg_run.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('qtagg')
+matplotlib.use('agg')
 from sqgturb import SQG, rfft2, irfft2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -43,7 +43,7 @@ dek = 0 # applied only at surface if symmetric=False
 nsq = 1.e-4; f=1.e-4; g = 9.8; theta0 = 300
 H = 10.e3 # lid height
 r = dek*nsq/f
-U = 16 # jet speed
+U = 20 # jet speed
 Lr = np.sqrt(nsq)*H/f # Rossby radius
 L = 20.*Lr
 # thermal relaxation time scale
@@ -77,13 +77,13 @@ model = SQG(pv,nsq=nsq,f=f,U=U,H=H,r=r,tdiab=tdiab,dt=dt,
 #  initialize figure.
 outputinterval = 6.*3600. # interval between frames in seconds
 tmin = 100.*86400. # time to start saving data (in days)
-tmax = 300.*86400. # time to stop (in days)
+tmax = 400.*86400. # time to stop (in days)
 nsteps = int(tmax/outputinterval) # number of time steps to animate
 # set number of timesteps to integrate for each call to model.advance
 ntimesteps = int(outputinterval/model.dt)
 savedata = 'sqgu%s_dek%s_N%s_6hrly.nc' % (U,dek,N) # save data plotted in a netcdf file.
 #savedata = None # don't save data
-plot = True # animate data as model is running?
+plot = False # animate data as model is running?
 
 if savedata is not None:
     from netCDF4 import Dataset
