519467bda80da39416f8f4d2d04373915d4f9ce0
Jeffrey S Whitaker <jwhitake@hercules-login-2.hpc.msstate.edu>
2026-01-25 17:58:23 -0600

    update

--- sqg_lgetkf_cvms.py
+++ sqg_lgetkf_cvms.py
@@ -67,8 +67,8 @@ read_restart = False
 savedata = None
 #nassim = 101
 #nassim_spinup = 1
-nassim = 1100 # assimilation times to run
-nassim_spinup = 100
+nassim = 1320 # assimilation times to run
+nassim_spinup = 120
 
 nanals = 16 # ensemble members
 ngroups = nanals//2  # number of groups for cross-validation (ngroups=nanals//N is "leave N out")
@@ -76,9 +76,9 @@ ngroups = nanals//2  # number of groups for cross-validation (ngroups=nanals//N 
 oberrstdev = 1. # ob error standard deviation in K
 
 # nature run created using sqg_run.py.
-filename_climo = 'sqgu20_N96_6hrly_12hdiff.nc' # file name for forecast model climo
+filename_climo = 'sqgu20_N64_6hrly_24hdiff.nc' # file name for forecast model climo
 # perfect model
-filename_truth = 'sqgu20_N96_6hrly_12hdiff.nc' # file name for nature run to draw obs
+filename_truth = 'sqgu20_N64_6hrly_24hdiff.nc' # file name for nature run to draw obs
 #filename_truth = 'sqg_N256_N96_12hrly.nc' # file name for nature run to draw obs
 
 if rank==0:
@@ -163,7 +163,7 @@ if rank==0:
 
 # each ob time nobs ob locations are randomly sampled (without
 # replacement) from the model grid
-nobs = 1024
+nobs = 820   
 
 # nature run
 if rank == 0:
