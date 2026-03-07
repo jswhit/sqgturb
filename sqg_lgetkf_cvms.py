1288b54e8f9a6495f145239c06d1c1d306a0bac5
jswhit2 <jwhitaker@Jeffreys-MacBook-Air.local>
2026-03-07 08:37:49 -0700

    updates from mslocal branch

--- sqg_lgetkf_cvms.py
+++ sqg_lgetkf_cvms.py
@@ -4,22 +4,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 from netCDF4 import Dataset
 import sys, time, os
-from sqgturb import SQG, rfft2, irfft2, cartdist, lgetkf, gaspcohn
+from sqgturb import SQG, rfft2, irfft2, cartdist, lgetkf_ms, gaspcohn
 
 # LGETKF cycling for SQG turbulence model with boundary temp obs,
-# ob space horizontal localization, no vertical localization.
+# multi-scale ob space horizontal localization, no vertical localization.
 # cross-validation update (no inflation).
 # Random observing network.
 
 if len(sys.argv) == 1:
    msg="""
 python sqg_lgetkf_cv.py hcovlocal_scale covinflate>
-   hcovlocal_scale = horizontal localization scale in km
+   hcovlocal_scales = horizontal localization scale(s) in km
+   band_cutoffs = filter waveband cutoffs 
+   crossbandcov_facts = cross-band covariance factors
    """
    raise SystemExit(msg)
 
 # horizontal covariance localization length scale in meters.
-hcovlocal_scale = float(sys.argv[1])
+hcovlocal_scales = eval(sys.argv[1])
+nlscales = len(hcovlocal_scales)
+band_cutoffs = eval(sys.argv[2])
+nband_cutoffs = len(band_cutoffs)
+if nband_cutoffs != nlscales-1:
+    raise SystemExit('band_cutoffs should be one less than hcovlocal_scales')
+crossbandcov_facts = eval(sys.argv[3])
+if len(crossbandcov_facts) != nband_cutoffs:
+    raise SystemExit('band_cutoffs and crossbandcov_facts should be same length')
+crossband_covmat = np.ones((nlscales,nlscales),np.float32)
+crossband_covmatr = np.ones((nlscales,nlscales),np.float32)
+for i in range(nlscales):
+    for j in range(nlscales):
+        if j != i:
+            crossband_covmat[j,i] = crossbandcov_facts[np.abs(i-j)-1] 
+            crossband_covmatr[j,i] = -crossbandcov_facts[np.abs(i-j)-1] 
+
 exptname = os.getenv('exptname','test')
 threads = int(os.getenv('OMP_NUM_THREADS','1'))
 
@@ -34,13 +52,12 @@ read_restart = False
 #savedata = 'restart'
 savedata = None
 #nassim = 101
-#nassim_spinup = 100
+#nassim_spinup = 1
 nassim = 1320 # assimilation times to run
 nassim_spinup = 120
 
 nanals = 16 # ensemble members
-nerger = True # use Nerger regularization for R localization
-ngroups = nanals//2  # number of groups for cross-validation (ngroups=nanals//n is "leave n out")
+ngroups = nanals//2  # number of groups for cross-validation (ngroups=nanals//N is "leave N out")
 
 oberrstdev = 1. # ob error standard deviation in K
 
@@ -93,13 +110,14 @@ for nanal in range(nanals):
     diff_order=nc_climo.diff_order,diff_efold=diff_efold,threads=threads))
 if read_restart: ncinit.close()
 
-hcovlocal_km = int(hcovlocal_scale/1000.)
-print("# hcovlocal=%g diff_efold=%s nanals=%s ngroups=%s" %\
-     (hcovlocal_km,diff_efold,nanals,ngroups))
+hcovlocal_scales_km = [lscale/1000. for lscale in hcovlocal_scales]
+print("# hcovlocal=%s diff_efold=%s nanals=%s ngroups=%s" %\
+     (repr(hcovlocal_scales_km),diff_efold,nanals,ngroups))
+print('# band_cutoffs=%s crossbandcov_facts=%s' % (repr(band_cutoffs),repr(crossbandcov_facts)))
 
 # each ob time nobs ob locations are randomly sampled (without
 # replacement) from the model grid
-nobs = 820
+nobs = 820 # number of obs to assimilate (randomly distributed)
 
 # nature run
 nc_truth = Dataset(filename_truth)
@@ -109,7 +127,7 @@ print('# random network nobs = %s' % nobs)
 
 oberrvar = oberrstdev**2*np.ones(nobs,np.float32)
 covlocal = np.empty((ny,nx),np.float32)
-covlocal_tmp = np.empty((nobs,nx*ny),np.float32)
+covlocal_tmp = np.empty((nlscales,nobs,nx*ny),np.float32)
 
 obtimes = nc_truth.variables['t'][:]
 if read_restart:
@@ -136,7 +154,9 @@ if savedata is not None:
    nc.L = models[0].L
    nc.H = models[0].H
    nc.nanals = nanals
-   nc.hcovlocal_scale = hcovlocal_scale
+   nc.hcovlocal_scales = hcovlocal_scales
+   nc.band_cutoffs = band_cutoffs
+   nc.crossbandcov_facts = crossband_covfacts
    nc.oberrstdev = oberrstdev
    nc.g = nc_climo.g; nc.theta0 = nc_climo.theta0
    nc.nsq = models[0].nsq
@@ -208,15 +228,15 @@ for ntime in range(nassim):
     xob = np.concatenate((x.ravel(),x.ravel()))[indxob]
     yob = np.concatenate((y.ravel(),y.ravel()))[indxob]
     # compute covariance localization function for each ob
-    for nob in range(nobs):
-        dist = cartdist(xob[nob],yob[nob],x,y,nc_climo.L,nc_climo.L)
-        covlocal = gaspcohn(dist/hcovlocal_scale)
-        covlocal_tmp[nob] = covlocal.ravel()
-        dist = cartdist(xob[nob],yob[nob],xob,yob,nc_climo.L,nc_climo.L)
+    for nl in range(nlscales):
+        for nob in range(nobs):
+            dist = cartdist(xob[nob],yob[nob],x,y,nc_climo.L,nc_climo.L)
+            covlocal = gaspcohn(dist/hcovlocal_scales[nl])
+            covlocal_tmp[nl,nob,...] = covlocal.ravel()
 
     # first-guess spread
-    pvensmean = pvens.mean(axis=0)
-    pvprime = pvens - pvensmean
+    pvensmean_b = pvens.mean(axis=0)
+    pvprime = pvens - pvensmean_b
 
     fsprd = (pvprime**2).sum(axis=0)/(nanals-1)
 
@@ -233,9 +253,40 @@ for ntime in range(nassim):
     obfits_b = (obfits**2).mean()
     obbias_b = obfits.mean()
     obsprd_b = obsprd.mean()
-    pvensmean_b = pvens.mean(axis=0).copy()
+    pvpert = pvens-pvensmean_b
     pverr_b = (scalefact*(pvensmean_b-pv_truth[ntime+ntstart]))**2
-    pvsprd_b = ((scalefact*(pvensmean_b-pvens))**2).sum(axis=0)/(nanals-1)
+    pvsprd_b = ((scalefact*pvpert)**2).sum(axis=0)/(nanals-1)
+
+    # filter background perturbations into different scale bands
+    if nlscales == 1:
+        pvens_filtered_lst=[pvpert]
+    else:
+        pvens_filtered_lst=[]
+        pvfilt_save = np.zeros_like(pvpert)
+        pvspec = rfft2(pvpert)
+        wavenums = models[0].wavenums[np.newaxis,np.newaxis,...]
+        for n,cutoff in enumerate(band_cutoffs):
+            #filtfact = np.exp(-(wavenums/cutoff)**8)
+            #pvfiltspec = filtfact*pvspec
+            pvfiltspec = np.where(wavenums < cutoff, pvspec, 0.+0.j)
+            pvfilt = irfft2(pvfiltspec)
+            pvens_filtered_lst.append(pvfilt-pvfilt_save)
+            #plt.figure()
+            #plt.imshow((pvfilt-pvfilt_save)[0,0,...],cmap=plt.cm.bwr)
+            #plt.title('scale = %s' % n)
+            pvfilt_save=pvfilt
+        pvsum = np.zeros_like(pvpert)
+        for n in range(nband_cutoffs):
+            pvsum += pvens_filtered_lst[n]
+        #plt.figure()
+        #plt.imshow((pvpert-pvsum)[0,0,...],cmap=plt.cm.bwr)
+        #plt.title('scale = %s' % nlscales)
+        pvens_filtered_lst.append(pvpert-pvsum)
+        #plt.show()
+        #raise SystemExit
+    pvens_filtered = np.asarray(pvens_filtered_lst)
+    pvens = np.dot(pvens_filtered.T,crossband_covmat).T
+    pvens += pvensmean_b  # mean added back to all scales.
 
     if savedata is not None:
         if savedata == 'restart' and ntime != nassim-1:
@@ -249,20 +300,31 @@ for ntime in range(nassim):
 
     # EnKF update
     # create 1d state vector.
-    xens = pvens.reshape(nanals,2,nx*ny)
+    # concatenate along ensemble dimension (nanals*nlscales)
+    xens = pvens.reshape(nlscales*nanals,2,nx*ny)
+    xmean = xens.mean(axis=0)
+    xprime = xens - xmean
+    hxprime = np.empty((nanals*nlscales,nobs),np.float32)
+    for nanal in range(nanals*nlscales):
+        hxprime[nanal] = (scalefact*xprime[nanal].reshape(2*nx*ny))[indxob] # surface pv obs
 
     # update state vector.
 
     # hxens,pvob are in PV units, xens is not
-    xens = lgetkf(xens,hxens,pvob,oberrvar,covlocal_tmp,nerger=nerger,ngroups=ngroups)
+    xens = lgetkf_ms(nlscales,xens,hxprime,pvob-hxensmean_b,oberrvar,covlocal_tmp,ngroups=ngroups)
 
     # back to 3d state vector
-    pvens = xens.reshape((nanals,2,ny,nx))
+    pvens = xens.reshape((nlscales*nanals,2,ny,nx))
+    pvensmean_a = pvens.mean(axis=0) 
+    pvens_filtered = pvens - pvensmean_a
+    pvens_filtered = pvens_filtered.reshape(nlscales,nanals,2,ny,nx)
+    pvprime = np.dot(pvens_filtered.T,crossband_covmatr).T
+    pvens = pvprime.sum(axis=0) + pvensmean_a
     t2 = time.time()
     if profile: print('cpu time for EnKF update',t2-t1)
 
     pvensmean_a = pvens.mean(axis=0)
-    pvprime = pvens-pvensmean_a
+    pvprime = pvens - pvensmean_a
     asprd = (pvprime**2).sum(axis=0)/(nanals-1)
     asprd_over_fsprd = asprd.mean()/fsprd.mean()
 
@@ -332,7 +394,8 @@ if ncount:
     wavenums = np.arange(ktotmax,dtype=np.float32)
     for n in range(1,ktotmax):
         print('# ',wavenums[n],pvspec_err[n],pvspec_sprd[n])
-    plt.loglog(wavenums[1:-1],pvspec_err[1:-1],color='r')
-    plt.loglog(wavenums[1:-1],pvspec_sprd[1:-1],color='b')
-    plt.title('error (red) and spread (blue) l=%s' % hcovlocal_km)
+    plt.loglog(wavenums[1:-1],pvspec_err[1:-1],color='r',label='error')
+    plt.loglog(wavenums[1:-1],pvspec_sprd[1:-1],color='b',label='spread')
+    plt.title('expt=%s' % exptname)
+    plt.legend()
     plt.savefig('errorspread_spectra_cv_%s.png' % exptname)
