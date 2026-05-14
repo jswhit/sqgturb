5430630cd72df7e10c202e2f2197f1f4e0978503
Jeff Whitaker <whitaker.jeffrey@gmail.com>
2026-05-14 21:11:35 +0000

    update

--- sqgturb/__init__.py
+++ sqgturb/__init__.py
@@ -19,5 +19,6 @@ Jeff Whitaker December, 2016 <jeffrey.s.whitaker@noaa.gov>
 from .enkf_utils import gaspcohn, cartdist, lgetkf, lgetkf_ms, lgetkf_vloc, lgetkf_ms_vloc, lgetkf_bloc, lgetkfms_bloc
 from .sqg import SQG, rfft2, irfft2
 from .sqg_mpi import SQG as SQG_mpi, fft_forward, fft_backward, newDistArrayGrid, newDistArraySpec, MPI
+from .sqg_jax import SQG as SQG_jax, SQGState
 
 __all__=['SQG','SQG_mpi','rfft2','irfft2',gaspcohn,cartdist,lgetkf,lgetkf_ms,lgetkf_vloc,lgetkf_ms_vloc,lgetkf_bloc,lgetkfms_bloc]
